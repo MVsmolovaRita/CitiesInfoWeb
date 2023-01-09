eb52c2838c9cdecf6ad9a0a7d5c0f2d0ea076b0d
Margarita Smolova <84370594+mvsmolova@users.noreply.github.com>
2023-01-09 15:49:57 +0800

    Add project files.

---
+++ CitiesInfoWeb/CitiesDataStore.cs
@@ -0,0 +1,38 @@
+﻿using CitiesInfoWeb.Models;
+
+namespace CitiesInfoWeb
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; }= new CitiesDataStore();
+        public List<City> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            Cities = new List<City>()
+            {
+                new City()
+                {
+                    Id = 1,
+                    Name = "New York",
+                    Description = "The most famous USA's city"
+                },
+
+
+                new City()
+                {
+                    Id = 2,
+                    Name = "London",
+                    Description = "The capital of the Great Britan"
+                },
+
+                new City()
+                {
+                    Id = 3,
+                    Name = "Sydney",
+                    Description = "Is not the capital of Australia"
+                }
+            };
+        }
+
+    }
+}
---
+++ CitiesInfoWeb/Controllers/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using CitiesInfoWeb;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController:ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+
+        {
+
+            return new JsonResult(
+
+                CitiesDataStore.Current.Cities
+                ) ;
+        }
+
+
+    }
+}
